6f9f9cfdfc91398ac69e49ae305355dc08db9ffe
tallguy188 <cksghd4220@naver.com>
2023-02-15 18:16:52 +0900

    upload file function

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -10,22 +10,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
 
     @Autowired
     private BoardService boardService;  //autowired로 boardservice를 주입
-    @GetMapping("/board/write")  //localhost:8090//board/write
+    @GetMapping("/board/write")  //localhost:8090//board/write,   GetMapping은 경로를 설정
     public String boardWriteForm() {
         return "boardwrite";
     }
 
 
-    @PostMapping("/board/writepro")  // GetMapping은 경로를 설정
-    public String boardWritePro(Board board , Model model){
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board , Model model, MultipartFile file) throws Exception{
 
-      boardService.write(board);
+      boardService.write(board,file);
 
       model.addAttribute("message","글작성이 완료되었습니다.");
         model.addAttribute("searchUrl","/board/list");
@@ -65,12 +66,12 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board,Model model) {
+    public String boardUpdate(@PathVariable("id") Integer id, Board board,Model model,MultipartFile file) throws Exception {
 
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
-        boardService.write(boardTemp);
+        boardService.write(boardTemp, file);
 
         model.addAttribute("message","수정이 완료되었습니다");
         model.addAttribute("searchUrl","/board/list");
--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -5,8 +5,11 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -15,7 +18,22 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     // 글 작성
-    public void write(Board board) {
+    public void write(Board board, MultipartFile file) throws Exception {   // html에서 넘어오는 name이랑 일치시켜야한다.
+        // 저장경로지정
+        String projectPath= System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        // 랜덤식별자생성
+        UUID uuid = UUID.randomUUID();
+
+        // 랜덤식별자 + 원래파일이름
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath,fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+
         boardRepository.save(board);
     }
 
