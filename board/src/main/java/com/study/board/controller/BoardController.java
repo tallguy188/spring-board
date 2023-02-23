2af196da9762423c66b56d84bcef9ecf8da0abaf
tallguy188 <cksghd4220@naver.com>
2023-02-23 19:43:02 +0900

    making list page

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,8 +41,18 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model) {   //데이터를 담아서 보이는 페이지로 보내주기 위해 model사용
-        model.addAttribute("list",boardService.boardList());
+    public String boardList(Model model,@PageableDefault(page = 0,size=10,sort="id",direction = Sort.Direction.DESC ) Pageable pageable) {//데이터를 담아서 보이는 페이지로 보내주기 위해 model사용
+
+        Page<Board> list = boardService.boardList(pageable);
+        // 페이지 블럭 처리
+        int nowPage = list.getPageable().getPageNumber() + 1; //pageable의 디폴트 값이 0부터 시작이라서 +1
+        int startPage = Math.max(nowPage - 4,1);  // 현재페이지가 4보다 작을때를 대비
+        int endPage = Math.min(nowPage + 5,list.getTotalPages()); //페이지가 토탈페이지수를 넘지 않도록 대비
+
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage",nowPage);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
 
         return "boardlist";
     }
